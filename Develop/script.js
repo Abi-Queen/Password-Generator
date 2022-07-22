e531e7232c8c103318178f9362bd259383d60a39
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-07-22 05:50:06 -0700

    input variables and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,61 @@
-// Assignment code here
-
-// prompt user welcome
-
 // prompt user choose length of pw from 8 to 128; validate input; store as var length; edge cases
+var length = window.prompt('Please choose a password length from 8 to 128 characters.')
+//change away from or to and, separate out
+if(length == string || length < 8 || length > 128) {
+  window.prompt('Please choose a length between 8 and 128 characters.')
+}
+console.log(length)
 
-// prompt user require letters; validate input; store as var numeric or var letters; edge cases
+// prompt user require letters/uppers/specials; validate input; store as var numeric or var letters; edge cases
+var letters = window.confirm('Should the password include letters?')
+console.log(letters)
 
-// prompt user require upper case letters; validate input; store as var lower or var upper; edge cases
+var upper = window.confirm('Should the password include upper case letters?')
+console.log(upper)
 
-// prompt user require special characters; validate input; store as var special or var no-special; edge cases 
+var special = window.confirm('Should the password include special characters?')
+console.log(special)
+
 
 // in pw generator function, set conditional statements for five types of pw: numeric only, alphanumeric only lower only, alphanumeric only upper and lower, alphanumeric lower only with special characters, alphanumeric lower and upper with special characters
+// Write password to the #password input
+// CITE SOURCE of function in readme
+function writePassword(length) {
+  
+  for (var i = 0; i < length; i++) {
 
-// plug conditionals into diff. pw engines within pw generator function; save outputs as password
+// create arrays for input pw options *QUESTION: where to put this?
+if (letters = 0, special = 0) {
+  var charInfo = [0123456789]
+}
+else if (letters = letters, upper = upper, special = 0) {
+  var charInfo = [0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]
+}
+else if (letters = 0, special = special) {
+  var charInfo = [0123456789,'`!@#$%^&*()_+~|}{][/-=.,:;"']
+}
+else if (letters = letters, upper = 0, special = special) {
+  var charInfo = [0123456789abcdefghijklmnopqrstuvwxyz'`!@#$%^&*()_+~|}{][/-=.,:;"']
+}
+else if (letters = letters, upper = 0, special = 0) {
+  var charInfo = [0123456789abcdefghijklmnopqrstuvwxyz]
+}
+else if (letters = letters, upper = upper, special = special) {
+  var charInfo = [0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,'`!@#$%^&*()_+~|}{][/-=.,:;"']
+}
+  }
+  password = characters.charInfo(Math.floor(Math.random()charactersLength));
 
-// prompt user to generate a new pw or cancel 
+//from starter code
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+}
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
