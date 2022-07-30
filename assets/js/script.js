3798f2ab598fb6afe45b89472c197cd55f7f44b1
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-07-30 12:11:54 -0700

    fixed bug and cleaned up comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,60 @@
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
+//Define arrays of possible character choices
 var length, upperCase, lowerCase, special, numbers;
 var upperCaseLettersArray = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
 var lowerCaseLettersArray = [
-  a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 var specialArray = [
   '!', '@', '#', '$', '%', '^', '&', '*'
 ]
 var numbersArray = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ]
-
-
-
-
-
 
 function generatePassword() {
 
   // prompt user choose length of pw from 8 to 128; convert to interger; store as var length; edge cases
-  length = number(window.prompt('Please choose a password length from 8 to 128 characters.'))
+  length = parseInt(prompt('Please choose a password length from 8 to 128 characters.'))
 
   //edge cases
   if (length < 8 || length > 128) {
     alert('Please choose a length between 8 and 128 characters.')
-    console.log(length)
   } else {
     lowerCase = confirm('Should the password include lower case letters?')
-    console.log(letters)
-
     upperCase = confirm('Should the password include upper case letters?')
-    console.log(upper)
-
     special = confirm('Should the password include special characters?')
-    console.log(special)
-
-    numbers = confirm()
+    numbers = confirm('Should the password include numbers?')
   }
 
+  //Plug in character requirements to arrays for sending to the for loop
   var possiblePasswordArray = [];
-  var passwordArray = [];
 
   if (lowerCase) {
     possiblePasswordArray = possiblePasswordArray.concat(lowerCaseLettersArray)
   }
-  // repeat for upper, special, numbers
   if (upperCase) {
-    possiblePasswordArray = possiblePasswordArray.concat(lowerCaseLettersArray)
+    possiblePasswordArray = possiblePasswordArray.concat(upperCaseLettersArray)
+  }
+  if (numbers) {
+    possiblePasswordArray = possiblePasswordArray.concat(numbersArray)
+  }
+  if (special) {
+    possiblePasswordArray = possiblePasswordArray.concat(specialArray)
   }
 
   var password = ''
-
 
   for (var i = 0; i < length; i++) {
     password += (possiblePasswordArray[Math.floor(Math.random() * possiblePasswordArray.length)])
   }
 
   return password;
-
 }
-
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -79,20 +66,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-// // STARTER CODE:
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
